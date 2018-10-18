f0abebf7a4df314cad0774437373d702edeee5dc
Tamnil Saito Junior <tamnil@gmail.com>
2018-10-18 06:39:40 -0300

    init

---
+++ extensions/code/Tamnil/Custom/Helper/Data.php
@@ -0,0 +1,16 @@
+<?php
+namespace Tamnil\Custom\Helper;
+
+use Magento\Catalog\Model\ResourceModel\Category\CollectionFactory;
+use Magento\Framework\App\Filesystem\DirectoryList;
+/**
+ * Webkul Xtremo Helper Data
+ */
+class Data extends \<...>\Helper\Data
+{
+    public function test()
+    {
+        return true;
+
+    }
+}
---
+++ extensions/code/Tamnil/Custom/registration.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Tamnil Saito Junior
+ *
+ * @category  Tamnil
+ * @package   Tamnil
+ * @author    Tamnil Software Private Limited
+ * @copyright Copyright (c) 2010-2018 
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Tamnil_Custom',
+    __DIR__
+);
